e824a7ded8e890254c7ae02aad4070c0b763bcee
Height Enterprise Consulting <jelasicj@gmail.com>
2025-08-18 03:25:42 -0400

    Update start.sh
    Added
    
    npx prisma migrate deploy || echo "No new migrations to apply"

--- start.sh
+++ start.sh
@@ -4,5 +4,6 @@
 # the server though, we need to run any prisma migrations that haven't yet been
 # run, which is why this file exists in the first place.
 # Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
+npx prisma migrate deploy || echo "No new migrations to apply"
 
 npm run start
